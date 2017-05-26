69fe508ea24aaf20b9df64cd28fc94249796388e
James Cammarata <jimi@sngx.net>
2017-05-26 16:08:24 -0500

    Adding architecture deep dive documentation

--- lib/ansible/cli/__init__.py
+++ lib/ansible/cli/__init__.py
@@ -753,18 +753,25 @@ class CLI(with_metaclass(ABCMeta, object)):
     @staticmethod
     def _play_prereqs(options):
 
+        # STARTDOC: architecture_deep_dive:ID:cli_data_loader
         # all needs loader
         loader = DataLoader()
+        # ENDDOC: architecture_deep_dive:ID:cli_data_loader
 
+        # STARTDOC: architecture_deep_dive:ID:cli_vault_pass
         vault_secrets = CLI.setup_vault_secrets(loader,
                                                 vault_ids=options.vault_ids,
                                                 vault_password_files=options.vault_password_files,
                                                 ask_vault_pass=options.ask_vault_pass)
         loader.set_vault_secrets(vault_secrets)
+        # ENDDOC: architecture_deep_dive:ID:cli_vault_pass
 
+        # STARTDOC: architecture_deep_dive:ID:cli_inventory
         # create the inventory, and filter it based on the subset specified (if any)
         inventory = InventoryManager(loader=loader, sources=options.inventory)
+        # ENDDOC: architecture_deep_dive:ID:cli_inventory
 
+        # STARTDOC: architecture_deep_dive:ID:cli_variable_manager
         # create the variable manager, which will be shared throughout
         # the code, ensuring a consistent view of global variables
         variable_manager = VariableManager(loader=loader, inventory=inventory)
@@ -772,5 +779,9 @@ class CLI(with_metaclass(ABCMeta, object)):
         # load vars from cli options
         variable_manager.extra_vars = load_extra_vars(loader=loader, options=options)
         variable_manager.options_vars = load_options_vars(options, CLI.version_info(gitinfo=False))
+        # ENDDOC: architecture_deep_dive:ID:cli_variable_manager
 
+        # STARTDOC: architecture_deep_dive:ID:cli_play_prereqs_return
         return loader, inventory, variable_manager
+        # ENDDOC: architecture_deep_dive:ID:cli_play_prereqs_return
+
